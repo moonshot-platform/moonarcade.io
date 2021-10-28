8bef69e901ae5af2a272dc243fab28086583c5bd
Light <light@Lights-MacBook-Pro.local>
2021-10-28 22:44:35 +0800

    initial commit - phaser included

---
+++ src/game.ts
@@ -0,0 +1,31 @@
+import 'phaser';
+
+export default class Demo extends Phaser.Scene
+{
+    constructor ()
+    {
+        super('demo');
+        
+    }
+
+    preload ()
+    {
+        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
+        this.load.glsl('stars', 'assets/starfields.glsl.js');
+    }
+
+    create ()
+    {
+        this.add.shader('RGB Shift Field', 0, 0, window.innerWidth, window.innerHeight).setOrigin(0);
+    }
+}
+
+const config = {
+    type: Phaser.AUTO,
+    backgroundColor: '#000000',
+    scene: Demo,
+    width: window.innerWidth,
+    height: window.innerHeight
+};
+
+const game = new Phaser.Game(config);
