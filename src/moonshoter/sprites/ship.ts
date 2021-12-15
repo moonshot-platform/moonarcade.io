2e8944a4b35277ed7ed6fecd6ba9142fd5b7ff1b
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2021-12-15 23:16:47 +0300

    Add ship sprite

---
+++ src/moonshoter/sprites/ship.ts
@@ -0,0 +1,9 @@
+import { AssetType } from "../interface/assets";
+
+export class Ship {
+    static create(scene: Phaser.Scene): Phaser.Physics.Arcade.Sprite {
+        let ship = scene.physics.add.sprite(window.screen.width / 2, window.screen.availHeight - 100, AssetType.Ship);
+        ship.setCollideWorldBounds(true);
+        return ship;
+    }
+}
