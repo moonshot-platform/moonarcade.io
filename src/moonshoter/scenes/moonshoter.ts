8fbe0f457f0e6d2564250e2b13e2888cf0f5022f
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2021-12-16 00:13:08 +0300

    Update moonshoter scene

--- src/moonshoter/scenes/moonshoter.ts
+++ src/moonshoter/scenes/moonshoter.ts
@@ -1,16 +1,194 @@
+import { AssetType, SoundType } from "../interface/assets";
+import { Bullet } from "../sprites/bullet";
+import { AssetManager } from "../manager/asset-manager";
+import { AlienManager } from "../manager/alien-manager";
+import { Ship } from "../sprites/ship";
+import {
+    AnimationFactory,
+    AnimationType,
+} from "../interface/factory/animation-factory";
+import { Alien } from "../sprites/alien";
+import { Kaboom } from "../sprites/kaboom";
+import { ScoreManager } from "../manager/score-manager";
+import { GameState } from "../interface/game-state";
+
 export class MoonshoterScene extends Phaser.Scene {
+    state: GameState;
+    assetManager: AssetManager;
+    animationFactory: AnimationFactory;
+    scoreManager: ScoreManager;
+    bulletTime = 0;
+    firingTimer = 0;
+    starfield: Phaser.GameObjects.TileSprite;
+    player: Phaser.Physics.Arcade.Sprite;
+    alienManager: AlienManager;
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+    fireKey: Phaser.Input.Keyboard.Key;
 
     constructor() {
         super({
-            key: "MoonshoterScene",
+            key: "MainScene",
         });
     }
 
-    preload() { }
+    preload() {
+        this.load.setBaseURL("/assets");
+        this.load.image(AssetType.Starfield, "/images/starfield.png");
+        this.load.image(AssetType.Bullet, "/images/bullet.png");
+        this.load.image(AssetType.EnemyBullet, "/images/enemy-bullet.png");
+        this.load.spritesheet(AssetType.Alien, "/images/invader.png", {
+            frameWidth: 32,
+            frameHeight: 32,
+        });
+        this.load.image(AssetType.Ship, "/images/player.png");
+        this.load.spritesheet(AssetType.Kaboom, "/images/explode.png", {
+            frameWidth: 128,
+            frameHeight: 128,
+        });
 
-    create() { }
+        this.sound.volume = 0.5;
+        this.load.audio(SoundType.Shoot, "/audio/shoot.wav");
+        this.load.audio(SoundType.Kaboom, "/audio/explosion.wav");
+        this.load.audio(SoundType.InvaderKilled, "/audio/invaderkilled.wav");
+    }
 
-    update() { }
+    create() {
+        this.state = GameState.Playing;
+        this.starfield = this.add
+            .tileSprite(0, 0, window.innerWidth, window.innerHeight, AssetType.Starfield)
+            .setOrigin(0, 0);
+        this.assetManager = new AssetManager(this);
+        this.animationFactory = new AnimationFactory(this);
+        this.cursors = this.input.keyboard.createCursorKeys();
+        this.fireKey = this.input.keyboard.addKey(
+            Phaser.Input.Keyboard.KeyCodes.SPACE
+        );
+        this.player = Ship.create(this);
+        this.alienManager = new AlienManager(this);
+        this.scoreManager = new ScoreManager(this);
 
-    restart() { }
+        this.fireKey.on("down", () => {
+            switch (this.state) {
+                case GameState.Win:
+                case GameState.GameOver:
+                    this.restart();
+                    break;
+            }
+        })
+    }
+
+    update() {
+        this.starfield.tilePositionY -= 1;
+        this._shipKeyboardHandler();
+        if (this.time.now > this.firingTimer) {
+            //this._enemyFires();
+        }
+
+        this.physics.overlap(
+            this.assetManager.bullets,
+            this.alienManager.aliens,
+            () => this._bulletHitAliens,
+            null,
+            this
+        );
+        this.physics.overlap(
+            this.alienManager.aliens,
+            this.player,
+            () => this._enemyHitPlayer,
+            null,
+            this
+        );
+    }
+
+    private _shipKeyboardHandler() {
+        let playerBody = this.player.body as Phaser.Physics.Arcade.Body;
+        playerBody.setVelocity(0, 0);
+        if (this.cursors.left.isDown) {
+            playerBody.setVelocityX(-200);
+        } else if (this.cursors.right.isDown) {
+            playerBody.setVelocityX(200);
+        }
+
+        if (this.fireKey.isDown) {
+            this._fireBullet();
+        }
+    }
+
+    private _bulletHitAliens(bullet: Bullet, alien: Alien) {
+        let explosion: Kaboom = this.assetManager.explosions.get();
+        bullet.kill();
+        alien.kill(explosion);
+        this.scoreManager.increaseScore();
+        if (!this.alienManager.hasAliveAliens) {
+            this.scoreManager.increaseScore(1000);
+            // this.scoreManager.setWinText();
+            // this.state = GameState.Win;
+        }
+        if (this.scoreManager.score > 1500) {
+            this.alienManager.alienVelocity = 500;
+        } else if (this.scoreManager.score > 1000) {
+            this.alienManager.alienVelocity = 400;
+        } else if (this.scoreManager.score > 500) {
+            this.alienManager.alienVelocity = 300;
+        } else if (this.scoreManager.score > 300) {
+            this.alienManager.alienVelocity = 200;
+
+        }
+    }
+
+    private _enemyHitPlayer(ship: Ship, enemy: Alien) {
+        let explosion: Kaboom = this.assetManager.explosions.get();
+        enemy.kill(explosion);
+        let live: Phaser.GameObjects.Sprite = this.scoreManager.lives.getFirstAlive();
+        if (live) {
+            live.setActive(false).setVisible(false);
+        }
+
+        explosion.setPosition(this.player.x, this.player.y);
+        explosion.play(AnimationType.Kaboom);
+        this.sound.play(SoundType.Kaboom)
+        if (this.scoreManager.noMoreLives) {
+            this.scoreManager.setGameOverText();
+            this.assetManager.gameOver();
+            this.state = GameState.GameOver;
+            this.player.disableBody(true, true);
+        }
+    }
+
+
+    // private _enemyFires() {
+    //     if (!this.player.active) {
+    //         return;
+    //     }
+    //     let enemyBullet: EnemyBullet = this.assetManager.enemyBullets.get();
+    //     let randomEnemy = this.alienManager.getRandomAliveEnemy();
+    //     if (enemyBullet && randomEnemy) {
+    //         enemyBullet.setPosition(randomEnemy.x, randomEnemy.y);
+    //         this.physics.moveToObject(enemyBullet, this.player, 120);
+    //         this.firingTimer = this.time.now + 2000;
+    //     }
+    // }
+
+    private _fireBullet() {
+        if (!this.player.active) {
+            return;
+        }
+
+        if (this.time.now > this.bulletTime) {
+            let bullet: Bullet = this.assetManager.bullets.get();
+            if (bullet) {
+                bullet.shoot(this.player.x, this.player.y - 18);
+                this.bulletTime = this.time.now + 200;
+            }
+        }
+    }
+
+    restart() {
+        this.state = GameState.Playing;
+        this.player.enableBody(true, this.player.x, this.player.y, true, true);
+        this.scoreManager.resetLives();
+        this.scoreManager.hideText();
+        this.alienManager.reset();
+        this.assetManager.reset();
+    }
 }
