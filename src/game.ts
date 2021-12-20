43c74e73ab73a7877b6768c6724a4fc787ffb302
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2021-12-20 15:02:26 +0300

    Change main scene in on game init

--- src/game.ts
+++ src/game.ts
@@ -1,27 +1,17 @@
 import 'phaser';
-
-export default class Demo extends Phaser.Scene {
-    constructor() {
-        super('demo');
-
-    }
-
-    preload() {
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
-    }
-
-    create() {
-        this.add.shader('RGB Shift Field', 0, 0, window.innerWidth, window.innerHeight).setOrigin(0);
-    }
-}
+import { MoonshoterScene } from './moonshoter/scenes/moonshoter';
 
 const config = {
+    title: "Moonshoter",
     type: Phaser.AUTO,
     backgroundColor: '#000000',
-    scene: Demo,
-    width: window.screen.availWidth,
-    height: window.screen.availHeight
+    scene: MoonshoterScene,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    physics: {
+        default: "arcade"
+    },
+    parent: "Moonshoters"
 };
 
 const game = new Phaser.Game(config);
