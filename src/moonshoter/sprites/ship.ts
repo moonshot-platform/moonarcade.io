4287207e0e7d46ae600e7fbc28fa6d66093b5177
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2021-12-20 15:31:31 +0300

    Set player center of screen

--- src/moonshoter/sprites/ship.ts
+++ src/moonshoter/sprites/ship.ts
@@ -2,7 +2,7 @@ import { AssetType } from "../interface/assets";
 
 export class Ship {
     static create(scene: Phaser.Scene): Phaser.Physics.Arcade.Sprite {
-        let ship = scene.physics.add.sprite(window.screen.width / 2, window.screen.availHeight - 100, AssetType.Ship);
+        let ship = scene.physics.add.sprite(window.innerWidth / 2, window.screen.availHeight - 100, AssetType.Ship);
         ship.setCollideWorldBounds(true);
         return ship;
     }
