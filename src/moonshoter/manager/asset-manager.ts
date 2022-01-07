7831313a8715006639bb12a2297dcae904598dc3
Furkan Koseoglu <furkankoseoglu65@gmail.com>
2022-01-08 02:40:39 +0300

    Set asset manager for enemy bullets

--- src/moonshoter/manager/asset-manager.ts
+++ src/moonshoter/manager/asset-manager.ts
@@ -1,4 +1,5 @@
 import { Bullet } from "../sprites/bullet";
+import { EnemyBullet } from "../sprites/enemy-bullet";
 import { Kaboom } from "../sprites/kaboom";
 
 export class AssetManager {
@@ -8,6 +9,7 @@ export class AssetManager {
 
     constructor(private _scene: Phaser.Scene) {
         this.bullets = this._createBullets();
+        this.enemyBullets = this._createEnemyBullets();
         this.explosions = this._createExplosions();
     }
 
@@ -18,9 +20,8 @@ export class AssetManager {
 
     reset() {
         this._createBullets();
+        this._createEnemyBullets();
     }
-
-
 
     private _createBullets(): Phaser.Physics.Arcade.Group {
         let bullets = this._scene.physics.add.group({
@@ -40,6 +41,16 @@ export class AssetManager {
         });
 
         return explosions;
+    }
+
+    private _createEnemyBullets(): Phaser.Physics.Arcade.Group {
+        let enemyBullets = this._scene.physics.add.group({
+            max: 0,
+            classType: EnemyBullet,
+            runChildUpdate: true
+        });
+        enemyBullets.setOrigin(0.5, 1);
+        return enemyBullets;
     }
 
 }
